6e6cb33c69ff0b2b4e01ebf3ca71729f62073d9b
Your Name <you@example.com>
2024-04-12 12:43:50 -0400

    now we are in the ballpark. results not great, but not useless either

--- agents.py
+++ agents.py
@@ -3,8 +3,8 @@ from tools import save_content, clinical_trials_search, human_tools
 from utils import print_agent_output, together_llm
 # Define your agents with roles and goals
 search_term_agent = Agent(
-    role='Medical Software Engineer',
-    goal='Comprehend a human prompt to generate a short search term to fetch most relevant clinical trials that a user can participate in',
+    role='Prompt Distiller',
+    goal='Extract the main keyword from the human prompt describing their medical condition.',
     backstory="""As a top medical expert and and software engineer, you will review
     a patient's prompt and distill their prompt into a searchable word or phrase. 
     This simple search term will then be passed as a url parameter to an api.
@@ -15,27 +15,25 @@ search_term_agent = Agent(
     llm=together_llm,
     max_iter=5,
     memory=False,
-    step_callback=lambda x: print_agent_output(x,"Senior Research Analyst Agent"),
-    tools=human_tools+[clinical_trials_search], # Passing human tools to the agent,
+    step_callback=lambda x: print_agent_output(x,"===search_term_agent==="),
+    tools=human_tools, # Passing human tools to the agent,
 )
 
-# trials_archiver_agent = Agent(
-#     role='Clinical Trials Analyst',
-#     goal='Interperate a human prompt to generate a short search term to fetch most relevant clinical trials that a user can participate in',
-#     backstory="""As a top medical expert and and software engineer, you will review
-#     a patient's prompt and distill their prompt into a searchable word or phrase using
-#     Essie expression syntax. This simple Essie expression will then be passed as a 
-#     url parameter to an api.
-#     """,
-#     verbose=True,
-#     allow_delegation=False,
-#     # llm=ClaudeHaiku,
-#     llm=together_llm,
-#     max_iter=5,
-#     memory=True,
-#     step_callback=lambda x: print_agent_output(x,"Senior Research Analyst Agent"),
-#     tools=[clinical_trials_search]+human_tools, # Passing human tools to the agent,
-# )
+trials_search_agent = Agent(
+    role='Clinical Trials Searcher',
+    goal='Use extracted keywords to find relevant clinical trials',
+    backstory="""As a top medical expert and and software engineer, you will use the key words
+    from the Prompt Distiller to fetch a list of all relevant clinical trials.
+    """,
+    verbose=True,
+    allow_delegation=False,
+    # llm=ClaudeHaiku,
+    llm=together_llm,
+    max_iter=5,
+    memory=True,
+    step_callback=lambda x: print_agent_output(x,"===trials_search_agent==="),
+    tools=[clinical_trials_search], # Passing human tools to the agent,
+)
 
 coordinator_agent = Agent(
     role='Clinical Trials Coordinator',
@@ -50,7 +48,7 @@ coordinator_agent = Agent(
     verbose=True,
     max_iter=5,
     memory=False,
-    step_callback=lambda x: print_agent_output(x,"Clinical Trials Coordinator"),
+    step_callback=lambda x: print_agent_output(x,"===coordinator_agent==="),
     allow_delegation=False,
     tools=human_tools
 )
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from crewai import Crew, Process
-from agents import search_term_agent, coordinator_agent 
+from agents import search_term_agent, coordinator_agent, trials_search_agent
 # archiver
 from tasks import get_search_term, get_relevant_clinical_trials, curate_the_list
 # saving_the_output
 from utils import print_agent_output
-
-
+import json
 
 # Instantiate your crew with a sequential process
 crew = Crew(
-    agents=[search_term_agent, coordinator_agent, 
+    agents=[search_term_agent, trials_search_agent, coordinator_agent, 
             # archiver
             ],
     tasks=[get_search_term,
@@ -27,6 +26,21 @@ crew = Crew(
 # Kick off the crew's work
 results = crew.kickoff()
 
+# def append_json_to_file(data, filename='output.txt'):
+    # """Append a dictionary as JSON to a file."""
+    # with open(filename, 'a') as file:
+    #     json.dump(data, file)
+    #     file.write('\n')  # Write a newline character after each JSON object
+
+
+def write_data_to_file(data, filename='output.txt'):
+    """Write data to a file, overwriting existing content."""
+    with open(filename, 'a') as file:
+        file.write(str(data))
+        file.write("\n")
 # Print the results
 print("Crew Work Results:")
 print(results)
+# print(json.dump(results))
+write_data_to_file(results)
+
